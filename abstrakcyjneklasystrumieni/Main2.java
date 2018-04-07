2cbffa55df13856e7bbc3ecc2e84a6b10cd2dc75
HelAsia <asiaheluszka1993@gmail.com>
2018-04-07 22:49:46 +0200

    Plik2 has been added to try some methods from File class

--- abstrakcyjneklasystrumieni/Main2.java
+++ abstrakcyjneklasystrumieni/Main2.java
@@ -2,30 +2,38 @@ package abstrakcyjneklasystrumieni;
 
 import java.io.*;
 
-public class Main2 
-{
-    public static void main(String[] args) throws IOException 
-    {
+public class Main2{
+    public static void main(String[] args) throws IOException{
         
-        File plik = new File("nazaPliku.txt");
+        File plik = new File("nazwaPliku.txt");
+        File plik2 = new File("test.txt");
         InputStream inS;
         OutputStream outS;
-        
         Reader reader;
+        
         Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(plik)));
-      
-        
-        
-        ((BufferedWriter)writer).write("lala");
+        Writer writer2 = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(plik2)));
 
+        ((BufferedWriter)writer).write("lala");
         ((BufferedWriter)writer).newLine();
         ((BufferedWriter)writer).write("nastepna linia");
         
         ((BufferedWriter)writer).close();
-    }
-    
-    void nazwaF(InputStream inS)
-    {
+        
+        ((BufferedWriter)writer2).write("lala");
+        ((BufferedWriter)writer2).newLine();
+        ((BufferedWriter)writer2).write("nastepna linia");
+        
+        ((BufferedWriter)writer2).close();
+        
+        boolean ifEquals = plik2.equals(plik);
+        System.out.println("We would like to check if both files are the same: " + ifEquals);
+        
+        String plikPath = plik.getAbsolutePath();
+        String plik2Path = plik2.getAbsolutePath();
+        System.out.println("Path to the plik is: " + plikPath);
+        System.out.println("Path to the plik2 is: " + plik2Path);
+        System.out.println(plik.getName());
         
     }
 }
